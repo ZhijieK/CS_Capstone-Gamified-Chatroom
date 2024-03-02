0bd9a64567ff1697ba5780c6d00eea7b0ae64d0c
Wei <83650106+wei892@users.noreply.github.com>
2024-03-02 18:42:03 -0500

    Revert "Revert "Profile Page" -- Module issue "

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import ShopPage from "./pages/ShopPage";
-import "./style.scss"
+import Profile from "./pages/Profile_Page";
+import "./style.scss";
 
 import { Link, useRoutes, Navigate } from "react-router-dom";
 import { AuthContext } from "./context/AuthContext.jsx";
@@ -25,11 +26,21 @@ function App() {
     {path: "register/", element: <Register />},
     {path: "chat/*", element: <Home />},
     {path: "shop_page/*", element: <ShopPage />},
+    {path: "profile/*", element: <Profile />},
   ])
   
 
   return (
     <div>
+      <Link to="/login"> Login </Link> 
+      <br></br>
+      <Link to="/register"> Register </Link>
+      <br></br>
+      <Link to="/chat"> Chat </Link>
+      <br></br>
+      <Link to="/shop_page"> Shop Page </Link>
+      <br></br>
+      <Link to="/profile"> Profile </Link>
       {linkElements}
     </div>
   );
