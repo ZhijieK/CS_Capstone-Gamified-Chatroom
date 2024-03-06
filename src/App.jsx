7a99fa505e18f0bb44f61cbc5aaf2d36566a07d7
Zhijie Kuang <zhijiekuangpersonal@gmail.com>
2024-03-06 18:29:46 -0500

    removed navigation bar at the top

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import ShopPage from "./pages/ShopPage";
-import Profile from "./pages/Profile_Page";
-import "./style.scss";
+import ProfilePage from './pages/Profile_Page.jsx';
+import "./style.scss"
 
 import { Link, useRoutes, Navigate } from "react-router-dom";
 import { AuthContext } from "./context/AuthContext.jsx";
@@ -24,23 +24,13 @@ function App() {
     {path: "/", element: <ProtectedRoute><Home /></ProtectedRoute>, index: true},
     {path: "login/", element: <Login />},
     {path: "register/", element: <Register />},
-    {path: "chat/*", element: <Home />},
-    {path: "shop_page/*", element: <ShopPage />},
-    {path: "profile/*", element: <Profile />},
+    {path: "shop_page/", element: <ShopPage />},
+    {path: "profile/", element: <ProfilePage />},
   ])
   
 
   return (
     <div>
-      <Link to="/login"> Login </Link> 
-      <br></br>
-      <Link to="/register"> Register </Link>
-      <br></br>
-      <Link to="/chat"> Chat </Link>
-      <br></br>
-      <Link to="/shop_page"> Shop Page </Link>
-      <br></br>
-      <Link to="/profile"> Profile </Link>
       {linkElements}
     </div>
   );
