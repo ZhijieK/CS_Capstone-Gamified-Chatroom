4b521b7ad9c334a7d06bbeb13cf335c2d9a41d6c
Wei <wlin.li2403@gmail.com>
2024-03-22 14:26:19 -0400

    setting up function for grabbing item info from db

--- src/components/profileComponents/Avatar.jsx
+++ src/components/profileComponents/Avatar.jsx
@@ -1,42 +1,60 @@
-import React, { useContext } from 'react';
-import { AuthContext } from '../../context/AuthContext.jsx';
-import { Link } from 'react-router-dom';
-import filler from '../images/generalIcons/filler.png'; /* Transparant img, placeholder for avatar before creation */
-import edit from '../images/generalIcons/edit.png';
+import React, { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../context/AuthContext.jsx";
+import { Link } from "react-router-dom";
+import filler from "../images/generalIcons/filler.png"; /* Transparant img, placeholder for avatar before creation */
+import edit from "../images/generalIcons/edit.png";
 
-const Avatar = ({userInfo}) => {
+const Avatar = ({ userInfo }) => {
+  const { currentUser } = useContext(AuthContext);
+  const inventoryInfo = userInfo.inventory;
+  const profileIconInfo = userInfo.profileIcon;
+  console.log(inventoryInfo, profileIconInfo);
 
-  const {currentUser} = useContext(AuthContext);
-  console.log(userInfo);
+  const getProfileItemsFromDatabase = async () => {
+    for (const item in profileIconInfo){
+      console.log(profileIconInfo[item]);
+    }
+  };  
+
+  useEffect(() =>{
+    getProfileItemsFromDatabase();
+  }, [])
 
   return (
-            <div className="container"> {/*Grey box on left side*/}
-                <div className="bar">
-                <Link to="../" style={{color: 'black'}}> <div className="button"> Go Back </div> </Link>
-                <p style={{fontSize: 35,padding:20}}>Profile</p>
-                <div className="button2"><img src={edit} /></div>
-                </div>
-                {/*The Avatar*/}
-                <div className="circle"> 
-                <img src={filler} id ='skin' />
-                <img src={filler} id ='mouth' />
-                <img src={filler} id='hair' />
-                <img src={filler} id='eyes' />
-                <img src={filler} id='shirt' />
-                </div>
-                {/*The Bio*/}
-                <div className="bio">
-                  <p style={{fontSize: 25, padding: 10}}>{currentUser.displayName}</p>
-                  <p style={{fontSize: 20}}>Bio: I'm cool</p>
-                </div>
-                <div className="stats">
-                  <p style={{fontSize: 20}}>Age: 23</p>
-                  <p style={{fontSize: 20}}>Friends: 12</p>
-                  <p style={{fontSize:25,padding:10}}>Gold: 100</p>
-                  <p style={{fontSize:25}}>Level 2: 300/500 xp</p>
-                </div>
-            </div>
-  )
-}
+    <div className="container">
+      {" "}
+      {/*Grey box on left side*/}
+      <div className="bar">
+        <Link to="../" style={{ color: "black" }}>
+          {" "}
+          <div className="button"> Go Back </div>{" "}
+        </Link>
+        <p style={{ fontSize: 35, padding: 20 }}>Profile</p>
+        <div className="button2">
+          <img src={edit} />
+        </div>
+      </div>
+      {/*The Avatar*/}
+      <div className="circle">
+        <img src={filler} id="skin" />
+        <img src={filler} id="mouth" />
+        <img src={filler} id="hair" />
+        <img src={filler} id="eyes" />
+        <img src={filler} id="shirt" />
+      </div>
+      {/*The Bio*/}
+      <div className="bio">
+        <p style={{ fontSize: 25, padding: 10 }}>{currentUser.displayName}</p>
+        <p style={{ fontSize: 20 }}>Bio: I'm cool</p>
+      </div>
+      <div className="stats">
+        <p style={{ fontSize: 20 }}>Age: 23</p>
+        <p style={{ fontSize: 20 }}>Friends: 12</p>
+        <p style={{ fontSize: 25, padding: 10 }}>Gold: 100</p>
+        <p style={{ fontSize: 25 }}>Level 2: 300/500 xp</p>
+      </div>
+    </div>
+  );
+};
 
-export default Avatar
+export default Avatar;
